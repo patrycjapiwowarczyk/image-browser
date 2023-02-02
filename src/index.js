0542bb20c3aaecfb135e53555380a8e368d47c4e
PatrycjaPiwowarczyk <piwowarczyk.p.a@gmail.com>
2023-02-02 21:24:16 +0100

    Task completed

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ const footer = document.querySelector('.footer');
 const lightbox = new SimpleLightbox('.gallery__imgbox a');
 
 let page = 1;
-let pagesNumber;
+let pagesNumber = 0;
 let lastSearched = null;
-let visibility = false;
+let smoothScrolling = false;
 
 async function fetchingImages(searchedInput, page) {
   try {
@@ -44,6 +44,21 @@ async function fetchingImages(searchedInput, page) {
     return response.data.hits;
   } catch (error) {
     console.log(error);
+  }
+
+  page = 1;
+}
+
+function scrolling() {
+  if (smoothScrolling) {
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
   }
 }
 
@@ -75,8 +90,33 @@ function updatingGallery(imgData) {
   });
 
   gallery.innerHTML += galleryCardHtml;
+  scrolling();
   lightbox.refresh();
+
+  if (page === 1 && pagesNumber !== 0) {
+    galleryButton.classList.remove('hidden');
+    smoothScrolling = true;
+  } else {
+    galleryButton.classList.add('hidden');
+  }
 }
+
+const footerObserver = new IntersectionObserver(async function (
+  entries,
+  observer
+) {
+  if (entries[0].isIntersecting === false) return;
+  if (page >= pagesNumber) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
+    return;
+  }
+  page += 1;
+  const imgData = await fetchingImages(lastSearched, page);
+  updatingGallery(imgData);
+
+  per_page = 40;
+  galleryButton.classList.remove('hidden');
+});
 
 const debouncing = debounce(async function () {
   const searchedInput = searchFormInput.value;
@@ -99,37 +139,10 @@ searchForm.addEventListener('submit', e => {
   debouncing();
 });
 
-function loadMoreImages (totalHits) {
-  let totalPages = totalHits / per_page;
-
-  if (totalPages > page || page === totalPages) {
-page =+ 1;
-visibility = true;
-galleryButton.classList.remove("hidden");
-galleryButton.addEventListener("click", debouncing);
-  }
-
-  else if (page > totalPages && totalPages !== 0) {
-    galleryButton.classList.add("hidden");
-    Notify.failure("We're sorry, but you've reached the end of search results.")
-  }
-
-  else if (page > totalPages) {
-    visibility = false;
-    galleryButton.classList.add("hidden");
-  }
-}
-
-function smoothScrolling () {
-  if (visibility) {
-    const { height: cardHeight } = document
-  .querySelector(".gallery")
-  .firstElementChild.getBoundingClientRect();
-
-window.scrollBy({
-  top: cardHeight * 2,
-  behavior: "smooth",
+galleryButton.addEventListener('click', async function () {
+  page += 1;
+  const imgData = await fetchingImages(lastSearched, page);
+  updatingGallery(imgData);
+  footerObserver.observe(footer);
+  galleryButton.classList.remove('hidden');
 });
-  }
-}
-
